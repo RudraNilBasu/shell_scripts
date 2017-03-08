72a546a884700260c0e55f2e5441428dba365bb3
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2017-03-08 22:45:45 +0530

    absolute directory

--- basics/directory_present.sh
+++ basics/directory_present.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # check if an input file/directory is present in the current directory
 
-echo -n "Enter a file/directory: "
-read dir
+echo -n "Enter a file/directory in the current Directory: "
+read absDir
 
+pwd=$(pwd)
+
+dir=$pwd"/"$absDir
+
+echo "$dir"
 if [ -d "$dir" ]; then
 	echo "Present: It is a Directory"
 elif [ -f "$dir" ]; then
