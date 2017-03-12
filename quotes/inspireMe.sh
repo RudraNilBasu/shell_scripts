a63487e26ad47c67459c6838a8b01c267d23ea9b
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2017-03-12 13:19:08 +0530

    lolcat

--- quotes/inspireMe.sh
+++ quotes/inspireMe.sh
@@ -2,4 +2,5 @@
 # write this at the end of the .bashrc file (if using bash) or .zshrc
 # (if using zsh)
 # sudo apt-get install cowsay
-shuf -n 1 quotes | cowsay
+# sudo apt-get install lolcat
+shuf -n 1 quotes | cowsay | lolcat
